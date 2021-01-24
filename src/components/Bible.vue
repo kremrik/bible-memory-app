7147f85354fca9dfd607f71771bc776ad976fdc9
kremrik <kyle.robert.e@gmail.com>
2021-01-23 20:00:40 -0600

    working hide hinting

--- src/components/Bible.vue
+++ src/components/Bible.vue
@@ -14,10 +14,6 @@
         <h1 class="display-2 font-weight-bold mb-3">
           Bible Memory App
         </h1>
-
-        <p class="subheading font-weight-regular">
-          A simple page for memorizing scripture
-        </p>
       </v-col>
 
       <v-col
@@ -44,18 +40,40 @@
           </v-col>
           <v-spacer/>
         </v-row>
-        <v-row>
-          <v-spacer/>
-          <v-col cols="7">
-            <p>{{ passageText }}</p>
-          </v-col>
-          <v-spacer/>
-        </v-row>
       </v-col>
-
+    </v-row>
+    <v-row>
+      <v-spacer/>
+      <v-col cols="8">
+        <p v-for="verse in passageText.verses" :key="verse.verse">
+          <v-hover 
+            v-slot:default="{ hover }"
+            open-delay="200"
+          >
+            <div>
+              <span>
+                [{{ verse.verse }}] {{ verse.hint }} 
+              </span>
+              <span :class="{ 'on-hover': !hover }">
+                {{ verse.rest }}
+              </span>
+            </div>
+          </v-hover>
+        </p>
+      </v-col>
+      <v-spacer/>
     </v-row>
   </v-container>
 </template>
+
+<style lang="scss" scoped>
+.on-hover{
+	color: transparent;
+	text-shadow: 0 0 8px #000;
+  transition: .4s ease-in-out;
+}
+
+</style>
 
 <script>
   // import queryBible from '@/utils/request';
@@ -64,15 +82,21 @@
     name: 'Bible',
     data: () => ({
       passage: "",
-      passageText: ""
+      passageText: "",
     }),
+    mounted: function() {
+    },
     methods: {
       async querybible() {
-        const url = `http://127.0.0.1:8001/passage/${this.passage}`;
+        const url = `http://127.0.0.1:8000/passage/${this.passage}`;
         const response = await fetch(url);
         const data = await response.json();
-        this.passageText = data.data;
+        this.passageText = data;
+        // let passage = data.verses.map(
+        //   x => `[${x.verse}] ${x.hint} ${x.rest}`
+        // );
+        // this.passageText = passage.join(" ");
       }
-    }
+    },
   }
 </script>
